7ad25888a170b387a84f9b290c2179fc1da47ee7
sachin kumar <sachinpatel.k.786@gmail.com>
2023-12-03 21:41:14 +0530

    HHHHGG

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const { connection } = require("./db")
 const { userRouter } = require("./Routes/userroutes")
 const { productRouter } = require("./Routes/productroutes")
 require('dotenv').config()
+const cors=require("cors")
 const app=express()
-
+app.use(cors())
 app.use(express.json())
 
 app.use("/user",userRouter)
